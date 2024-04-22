0bb750af569e4e5fb3cd516fe7d47c1a37f30e49
Yao Lu Home Win11 <luyao2493751@gmail.com>
2024-04-23 01:25:46 +0300

    initial commit

---
+++ ramenHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ramenHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
