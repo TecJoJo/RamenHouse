53909154983484e97928fc670c1c4916032aff14
Yao Laptop <luyao2493751@gmail.com>
2024-05-26 19:36:18 +0300

    meal create halfway

--- ramenHouse/Controllers/AdminController.cs
+++ ramenHouse/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ramenHouse.FormModels;
 using ramenHouse.Models;
@@ -9,10 +10,14 @@ namespace ramenHouse.Controllers
     public class AdminController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly IConfiguration _config;
+        private readonly IWebHostEnvironment _env;
 
-        public AdminController(ApplicationDbContext dbContext)
+        public AdminController(ApplicationDbContext dbContext, IConfiguration config, IWebHostEnvironment env)
         {
             _dbContext = dbContext;
+            _config = config;
+            _env = env;
         }
         public IActionResult Index()
         {
@@ -69,6 +74,78 @@ namespace ramenHouse.Controllers
 
 
             return RedirectToAction("Index");   
+        }
+
+        [HttpGet]
+        public IActionResult MealCreate()
+        {
+            var allergies = _dbContext.Allergies.ToList();
+            ViewData["allergies"] = allergies;
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MealCreate(MealCreateFormModel mealCreateForm)
+        {
+            if(ModelState.IsValid)
+            {
+                
+
+                //store the uploaded image 
+                var imgStoragePath = Path.Combine(_env.WebRootPath, _config["imgStoragePath"]);
+                if (!Directory.Exists(imgStoragePath))
+                {
+                    Directory.CreateDirectory(imgStoragePath);
+                }
+
+                string filename = Path.GetFileNameWithoutExtension(mealCreateForm.imgFile.FileName);
+                string fileExtension = Path.GetExtension(mealCreateForm.imgFile.FileName);
+                filename = filename + DateTime.Now.ToString("yymmssfff") + fileExtension;
+
+                var filePath = Path.Combine(imgStoragePath,filename);
+                // we extract this relative path which can be using in the html. 
+                string relativeFilePath = Path.Combine(_config["imgStoragePath"], Path.GetFileName(filePath)).Replace("\\", "/");
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    mealCreateForm.imgFile.CopyTo(stream);
+                }
+
+                //add the new meal infomation into the database
+                var newMeal = new Meal()
+                {
+                    Title = mealCreateForm.Name,
+                    Description = mealCreateForm.Description,
+                    ImgUrl = relativeFilePath,
+                    BasePrice = mealCreateForm.BasePrice,
+                    CreationTime = DateTime.Now,
+
+                    //we temparatyly add fixed category as the time is running out
+                    CategoryId = _dbContext.Categories.FirstOrDefault().CategoryId
+
+
+                };
+
+                //add allergies into the meal's allergy's collection
+
+                foreach(int allergyId in mealCreateForm.AllergyIds)
+                {
+                    Allergy newAllergy = _dbContext.Allergies.Find(allergyId);
+                    if (newAllergy != null)
+                    {
+                        newMeal.Allergies.Add(newAllergy);
+                    }
+
+                }
+
+                _dbContext.Meals.Add(newMeal);
+                _dbContext.SaveChanges();
+
+
+                
+            }
+
+            return RedirectToAction("MealCreate");
         }
     }
 }
---
+++ ramenHouse/FormModels/MealCreateFormModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ramenHouse.FormModels
+{
+    public class MealCreateFormModel
+    {
+       
+            public string Name { get; set; }
+            public bool IsFeatured { get; set; }
+            public decimal SellPrice { get; set; }
+            public decimal BasePrice { get; set; }
+            public decimal Discount { get; set; }
+            public List<int> AllergyIds { get; set; }
+            public string Description { get; set; }
+            public IFormFile imgFile { get; set; }
+            //tempararylly leave the category blank, because of the complexity reason
+        
+
+    }
+}
